87de99780b61f9de67c2e7a6feb94cb1ca010f83
Kholod Sergey <kholodsergey@yandex.ru>
2020-03-27 19:26:07 +0500

    Update test2

--- src/test/java/ru/netology/request/RequestTest.java
+++ src/test/java/ru/netology/request/RequestTest.java
@@ -1,7 +1,6 @@
 package ru.netology.request;
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
